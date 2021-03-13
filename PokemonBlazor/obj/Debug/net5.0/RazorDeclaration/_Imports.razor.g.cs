273c873b02f10a32f376c13da38a9bdf4d525a9b
Junior Felix Gervacio Burgos <juniorfelixgb@gmail.com>
2021-03-13 19:52:59 -0400

    update project

--- PokemonBlazor/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ PokemonBlazor/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -12,6 +12,76 @@ namespace PokemonBlazor
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
+#nullable restore
+#line 1 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using System.Net.Http;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using System.Net.Http.Json;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using Microsoft.AspNetCore.Components.Forms;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 4 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using Microsoft.AspNetCore.Components.Routing;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 5 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using Microsoft.AspNetCore.Components.Web;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 6 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 7 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 8 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using Microsoft.JSInterop;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 9 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using PokemonBlazor;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 10 "c:\Users\Junior Gervacio\Desktop\Github\PokemonWork\Pokemon\PokemonBlazor\_Imports.razor"
+using PokemonBlazor.Shared;
+
+#line default
+#line hidden
+#nullable disable
     public partial class _Imports : System.Object
     {
         #pragma warning disable 1998
